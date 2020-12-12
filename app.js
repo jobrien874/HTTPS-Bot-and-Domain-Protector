e1a3f8c2864171e72a5fa2ded398d73fe3f14a84
Joshua O'Brien <josh@deveire.com>
2020-12-12 22:38:12 +0000

    Dotenv for local

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const dotenv = require('dotenv');
-const result = dotenv.config()
+/* const dotenv = require('dotenv');
+const result = dotenv.config() */
 var check = require('./check');
 
 // Scrounger Bot V1.0 Init
-if (result.error) {
+/* if (result.error) {
   throw result.error
-}
+} */
+
+//dotenv for local
 
 check.SecurityChecker()
