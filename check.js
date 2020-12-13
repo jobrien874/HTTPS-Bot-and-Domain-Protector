747896eb523b60d12d11edbb169d296e83467728
Joshua O'Brien <josh@deveire.com>
2020-12-13 11:00:09 +0000

    Edits

--- check.js
+++ check.js
@@ -7,32 +7,37 @@ console.log(twitterConfig.twitterApp)
 const Tweeter = new Twit(twitterConfig.twitterApp)
 
 exports.SecurityChecker = function() {
-   let hits = 0
+  var hits = 0
+  let itemsProcessed = 0
   let list = clients.clientList()
 
    list.forEach(element => {
-    console.log(element)
     sslChecker(element.domain, 'GET', 443).then(result => {
       if(result.valid === false) {
-        console.log(result)
         let howManySites
         howManySites = result.validFor.length
         hits++
+        itemsProcessed++
         let https = 'https://' + element.domain
         let message = mailer.messageMaker(https, howManySites)
-        console.log(message)
-        if(element.email) {
-          mailer.sendEmail(element.email, message) // switch test email to element.email
+         if(element.email) {
+          mailer.sendEmail('jobrien874@gmail.com', message) // switch test email to element.email
         } else {
           // send a direct message
-          var params = {event: {type:"message_create", message_create: { target: { recipient_id: element.twitterID }, message_data: { text: message}}}} // switch recipient_id to element.twitterID
-          Tweeter.post('direct_messages/events/new', params, function(err, data, response) { console.log(data)})
+/*           var params = {event: {type:"message_create", message_create: { target: { recipient_id: element.twitterID }, message_data: { text: message}}}} // switch recipient_id to element.twitterID
+          Tweeter.post('direct_messages/events/new', params, function(err, data, response) { console.log(data)}) */
         }
         // send the lads a tweet/email
         console.log('expired!')
+        if(itemsProcessed === list.length) {
+          sendReportToMe();
+        }
       }
     }
     );
   });
-  mailer.sendEmail('jobrien874@gmail.com', 'This many hits today! ' + hits)
+
+  function sendReportToMe () {
+     mailer.sendEmail('jobrien874@gmail.com', 'This many hits today! ' + hits)
+  }
 }
