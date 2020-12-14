0a48870d4ee76f4699a0ce631f75d49de67c1373
Joshua O'Brien <josh@deveire.com>
2020-12-14 20:38:36 +0000

    Tests

--- test/testCheck.js
+++ test/testCheck.js
@@ -8,14 +8,62 @@ exports.SecurityChecker = function () {
 /*   let itemsProcessed = 0;
   let list = clients.clientList(); */
   let query = {
-    "domain": 'google.com',
+    "domain": 'joshobrien.eu',
     "checkType": 'FAST',
     "forTransfer": false
   }
 
   godaddy.domains.available(query).then(function(res){
-    console.log('run')
-    console.log(res.body)
+    console.log('ScroungerBot V2.0 Running the Domain Expiry Check....');
+    console.log(res.body);
+    let power = Math.pow(10, 6)
+    let price = res.body.price / power;
+    console.log(price);
+    let result = res.body;
+    if(result.available) {
+      console.log('Available!');
+
+      if(price < 20) {
+        console.log('yes')
+      }else {
+        console.log('no')
+      }
+ /*      let query2 = {
+        "xShopperId": '328453142'
+      }
+      // buy it and let me know its a hit
+      godaddy.domains.purchase(query2).then(function(res){
+        console.log(res.body)
+      }); */
+
+      let query3 = {
+        "tld": 'com'
+      }
+
+      godaddy.domains.schema(query3).then(function(result){
+        console.log('check')
+        console.log(result.body)
+      });
+    } else {
+      let query2 = {
+        "xShopperId": '328453142',
+        "body": "body"
+      }
+
+      let query3 = {
+        "tld": 'joshobrien.org'
+      }
+      console.log(godaddy.domains.schema(query3));
+      godaddy.domains.schema(query3).then(function(res){
+        console.log(res)
+      });
+
+/*       console.log(godaddy.domains.purchase(query2));
+      godaddy.domains.purchase(query2).then(function(res){
+        console.log(res)
+      }); */
+      // let me know its not available and not a hit
+    }
   })
 
 /*   list.forEach((element) => {
